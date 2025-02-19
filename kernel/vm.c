7601a7c61813742b18425d7830733e08ec231c08
shyeokchoi <shyeokchoi@gmail.com>
2025-02-20 00:51:13 +0900

    copyout done

--- kernel/vm.c
+++ kernel/vm.c
@@ -402,8 +402,15 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
     if(va0 >= MAXVA)
       return -1;
     pte = walk(pagetable, va0, 0);
-    if (pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0 || (*pte & PTE_W) == 0)
+    if (pte == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_U) == 0 || ((*pte & PTE_W) == 0 && (*pte & PTE_COW) == 0))
       return -1;
+
+    if (*pte & PTE_COW) {
+      if (handle_cow(pagetable, va0) < 0) {
+        return -1;
+      }
+    }
+
     pa0 = PTE2PA(*pte);
     n = PGSIZE - (dstva - va0);
     if(n > len)
