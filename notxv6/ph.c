5f85e8bfef3a33cb19e76685c500c3d84cc3d91e
shyeokchoi <shyeokchoi@gmail.com>
2025-05-11 14:20:49 +0900

    Using threads

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -11,100 +11,102 @@
 struct entry {
   int key;
   int value;
-  struct entry *next;
+  struct entry* next;
 };
-struct entry *table[NBUCKET];
+
+struct bucket {
+  pthread_mutex_t lock;
+  struct entry* head;
+};
+
+struct bucket table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
-
-double
-now()
+double now()
 {
- struct timeval tv;
- gettimeofday(&tv, 0);
- return tv.tv_sec + tv.tv_usec / 1000000.0;
+  struct timeval tv;
+  gettimeofday(&tv, 0);
+  return tv.tv_sec + tv.tv_usec / 1000000.0;
 }
 
-static void 
-insert(int key, int value, struct entry **p, struct entry *n)
+static void insert(int key, int value, struct bucket* b)
 {
-  struct entry *e = malloc(sizeof(struct entry));
+  struct entry* e = malloc(sizeof(struct entry));
   e->key = key;
   e->value = value;
-  e->next = n;
-  *p = e;
+  e->next = b->head;
+  b->head = e;
 }
 
-static 
-void put(int key, int value)
+static void put(int key, int value)
 {
   int i = key % NBUCKET;
 
   // is the key already present?
-  struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key)
-      break;
-  }
-  if(e){
-    // update the existing key.
-    e->value = value;
-  } else {
-    // the new is new.
-    insert(key, value, &table[i], table[i]);
+  struct bucket* b = &table[i];
+  pthread_mutex_lock(&b->lock);
+
+  struct entry* e = 0;
+  for (e = b->head; e != 0; e = e->next) {
+    if (e->key == key) {
+      // update the existing key.
+      e->value = value;
+      pthread_mutex_unlock(&b->lock);
+      return;
+    }
   }
 
+  // insert new one
+  insert(key, value, &table[i]);
+  pthread_mutex_unlock(&b->lock);
 }
 
-static struct entry*
-get(int key)
+static struct entry* get(int key)
 {
   int i = key % NBUCKET;
 
-
-  struct entry *e = 0;
-  for (e = table[i]; e != 0; e = e->next) {
-    if (e->key == key) break;
+  struct entry* e = 0;
+  for (e = table[i].head; e != 0; e = e->next) {
+    if (e->key == key) {
+      break;
+    }
   }
 
   return e;
 }
 
-static void *
-put_thread(void *xa)
+static void* put_thread(void* xa)
 {
-  int n = (int) (long) xa; // thread number
-  int b = NKEYS/nthread;
+  int n = (int)(long)xa; // thread number
+  int b = NKEYS / nthread;
 
   for (int i = 0; i < b; i++) {
-    put(keys[b*n + i], n);
+    put(keys[b * n + i], n);
   }
 
   return NULL;
 }
 
-static void *
-get_thread(void *xa)
+static void* get_thread(void* xa)
 {
-  int n = (int) (long) xa; // thread number
+  int n = (int)(long)xa; // thread number
   int missing = 0;
 
   for (int i = 0; i < NKEYS; i++) {
-    struct entry *e = get(keys[i]);
-    if (e == 0) missing++;
+    struct entry* e = get(keys[i]);
+    if (e == 0)
+      missing++;
   }
   printf("%d: %d keys missing\n", n, missing);
   return NULL;
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-  pthread_t *tha;
-  void *value;
+  pthread_t* tha;
+  void* value;
   double t1, t0;
-
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
@@ -118,33 +120,36 @@ main(int argc, char *argv[])
     keys[i] = random();
   }
 
+  for (int i = 0; i < NBUCKET; ++i) {
+    table[i].head = 0;
+    pthread_mutex_init(&table[i].lock, NULL);
+  }
+
   //
   // first the puts
   //
   t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, put_thread, (void *) (long) i) == 0);
+  for (int i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, put_thread, (void*)(long)i) == 0);
   }
-  for(int i = 0; i < nthread; i++) {
+  for (int i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   t1 = now();
 
-  printf("%d puts, %.3f seconds, %.0f puts/second\n",
-         NKEYS, t1 - t0, NKEYS / (t1 - t0));
+  printf("%d puts, %.3f seconds, %.0f puts/second\n", NKEYS, t1 - t0, NKEYS / (t1 - t0));
 
   //
   // now the gets
   //
   t0 = now();
-  for(int i = 0; i < nthread; i++) {
-    assert(pthread_create(&tha[i], NULL, get_thread, (void *) (long) i) == 0);
+  for (int i = 0; i < nthread; i++) {
+    assert(pthread_create(&tha[i], NULL, get_thread, (void*)(long)i) == 0);
   }
-  for(int i = 0; i < nthread; i++) {
+  for (int i = 0; i < nthread; i++) {
     assert(pthread_join(tha[i], &value) == 0);
   }
   t1 = now();
 
-  printf("%d gets, %.3f seconds, %.0f gets/second\n",
-         NKEYS*nthread, t1 - t0, (NKEYS*nthread) / (t1 - t0));
+  printf("%d gets, %.3f seconds, %.0f gets/second\n", NKEYS * nthread, t1 - t0, (NKEYS * nthread) / (t1 - t0));
 }
